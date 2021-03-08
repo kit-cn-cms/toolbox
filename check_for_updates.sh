1c70fd73f19858e05c90d5fe826c85d64c471fd7
Jan van der Linden <jan.linden@student.kit.edu>
2021-03-08 15:10:26 +0100

    commit printing doesnt work, remove it again

--- check_for_updates.sh
+++ check_for_updates.sh
@@ -4,20 +4,6 @@ then
 
     if read -q "choice?Do you want to update TOOLBOX? [y/N] ";
     then
-        echo "\ncommits to be updated:"
-
-        # collecting all commits
-        IFS=$'\n' commits=($(git log --pretty=oneline origin master))
-        for commit in $commits
-        do
-            IFS=' ' read -rA c <<< "$commit"
-            # print all commits up to the one that is currently active
-            if [[ $(git rev-parse HEAD) == $c[1] ]]
-            then
-                break
-            fi
-            echo $commit
-        done
         echo "\nupdating..."
         git pull origin master
         echo "done."
