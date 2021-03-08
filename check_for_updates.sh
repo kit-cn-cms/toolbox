dee84c1ad10b69b5a7376fb1d76b08960917682d
Jan van der Linden <jan.linden@student.kit.edu>
2021-03-08 14:01:06 +0100

    updated check script to show commits that are updated

--- check_for_updates.sh
+++ check_for_updates.sh
@@ -5,8 +5,23 @@ then
     if read -q "choice?Do you want to update TOOLBOX? [y/N] ";
     then
         echo "\nupdating..."
+        echo "\tcommits to be updated:"
+
+        # collecting all commits
+        IFS=$'\n' commits=($(git log --pretty=oneline origin master))
+        for commit in $commits
+        do
+            IFS=' ' read -rA c <<< "$commit"
+            # print all commits up to the one that is currently active
+            if [[ $(git rev-parse HEAD) == $c[1] ]]
+            then
+                break
+            fi
+            echo $commit
+        done
+        echo "\npulling..."
         git pull origin master
-        echo "done..."
+        echo "done."
     fi
 fi
 cd -
